719aa8606900f777cd4b2b3b5c6a1f4312b72bc0
Thiago <thiago.fukushima@7comm.com.br>
2021-09-20 13:20:14 -0300

    logs com winston

--- index.ts
+++ index.ts
@@ -1,8 +1,24 @@
 import express from 'express';
-// rest of the code remains same
+import winston from 'winston';
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with level `error` and below to `error.log`
+    // - Write all logs with level `info` and below to `combined.log`
+    //
+    new winston.transports.File({ filename: './logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: './logs/combined.log' }),
+  ],
+});
+
 const app = express();
 const PORT = 8000;
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+  logger.info('Hello again distributed logs');
 });
